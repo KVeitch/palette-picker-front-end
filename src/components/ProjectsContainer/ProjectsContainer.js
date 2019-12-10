addd47e2f2b12163cc6daddf83300833820707ad
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-12-09 21:57:19 -0700

    Add ProjectsContainer component

---
+++ src/components/ProjectsContainer/ProjectsContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Project from '../Project/Project'
+
+function ProjectsContainer(props) {
+  // const projects = this.props.projects.map()
+  return (
+    <div>
+      <Project />
+    </div>
+  );
+}
+
+export default ProjectsContainer;
