55f88b5643cd9476864b5304fc5b94e705d3e200
ericwm76 <eric.meldrum@gmail.com>
2019-12-10 16:17:51 -0700

    Create fetch functions for getting and posting projects and palettes

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,10 +1,59 @@
 const baseUrl = 'https://api-palette-picker.herokuapp.com/api/v1'
 
 export const getAllPalettes = async () => {
-  let response = await fetch(`${baseUrl}/palettes`);
+  const response = await fetch(`${baseUrl}/palettes`);
   if(!response.ok) {
     console.error('Could not get palettes.')
-  } 
-  let data = await response.json();
-  console.log(data)
+  }
+  const data = await response.json();
+  return data;
+}
+
+export const getPaletteById = async id => {
+  const response = await fetch(`${baseUrl}/palettes/${id}`);
+  const data = response.json();
+  return data;
+}
+
+export const getAllProjects = async () => {
+  const response = await fetch(`${baseUrl}/projects`);
+  if(!response.ok) {
+    console.error('Could not get projects.')
+  }
+  const data = await response.json();
+  return data;
+}
+
+export const getProjectById = async id => {
+  const response = await fetch(`${baseUrl}/projects/${id}`);
+  const data = response.json();
+  return data;
+}
+
+export const postProject = async (projectInfo) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(projectInfo)
+  };
+
+  const response = await fetch(`${baseUrl}/projects`, options);
+  const data = response.json();
+  return data;
+}
+
+export const postPalette = async (paletteInfo) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(paletteInfo)
+  };
+
+  const response = await fetch(`${baseUrl}/palettes`, options);
+  const data = response.json();
+  return data;
 }
