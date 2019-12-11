2bffafa48fcb11b9966880572277eb12e164c80d
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-12-10 23:26:36 -0700

    Add snapshot test for Project component

--- src/components/ProjectsContainer/ProjectsContainer.js
+++ src/components/ProjectsContainer/ProjectsContainer.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Project from '../Project/Project'
 
 function ProjectsContainer(props) {
-  const projects = props.projects.map(project => {
+  const projects = props.projects.map((project, index) => {
     let { palettes, project_name } = project;
-    return <Project palettes={palettes} project_name={project_name}/>
+    return <Project palettes={palettes} project_name={project_name} key={'Project'+ index}/>
   })
   return (
     <section className="section__projects--container">
