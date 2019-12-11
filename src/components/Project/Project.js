ccb5189c2bfe35d7c5e9f1d3382e9c635071de4d
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-12-11 15:43:38 -0700

    Add conditional  rendering if there are no palettes

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -5,7 +5,7 @@ import bin from '../../images/bin.svg'
 
 function Project(props) {
   const { palettes, project_name, project_id, removeProject} = props
-  const displayPalettes = palettes.map((palette,index) =><Palette palette={palette} key={'palette' + index}/>)
+  const displayPalettes = palettes ? palettes.map((palette,index) =><Palette palette={palette} key={'palette' + index}/>): ''
   return (
     <div className="div__project">
       <div>
