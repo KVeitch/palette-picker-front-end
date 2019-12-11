56df821b2b94b728ee24d9528e8e0c1702fc308f
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-12-10 20:21:41 -0700

    Adds an initial project  for styling and props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,8 +11,15 @@ class App extends Component {
   constructor() {
     super()
     this.state= {
-      user: {},
-      projects: [{project_name: 'Dining Room', project_id:1}],
+      user: {id:1, user_name:'Bob'},
+      projects: [{project_name: 'Dining Room', project_id:1, 
+        palettes:[
+          {"id": 1,"palette_name": "Colors","project_id": 1,"color0": "6320ee","color1": "D5f2e3","color2": "999950","color3": "C7B446","color4": "C2B078"},
+          {"id": 2,"palette_name": "Bright","project_id": 1,"color0": "786fa6","color1": "f19066","color2": "FFFF00","color3": "EDFF21","color4": "A98307"},
+          {"id": 3,"palette_name": "Joker","project_id": 1,"color0": "211a1d","color1": "6320ee","color2": "D5f2e3","color3": "8075ff","color4": "8fc93a"},
+          {"id": 4,"palette_name": "Russian Flat","project_id": 1,"color0": "f19066","color1": "786fa6","color2": "f19066","color3": "786fa6","color4": "574b90"},
+          {"id": 5,"palette_name": "For Bill","project_id": 1,"color0": "16a085","color1": "2ecc71","color2": "AEA04B","color3": "E6D690","color4": "CDA434"}
+        ]}],
       currentProjectId: null,
       color0: { hex:'4f4f4f', isLocked:false},
       color1: { hex:'1c6a77', isLocked:false},
