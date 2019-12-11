c131be1ad12ff57f73e3fef05181c4138ca5f213
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-12-11 15:45:58 -0700

    Format code

--- src/components/ProjectsContainer/ProjectsContainer.js
+++ src/components/ProjectsContainer/ProjectsContainer.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import Project from '../Project/Project'
+import Project from '../Project/Project';
 
 function ProjectsContainer(props) {
   const projects = props.projects.map((project, index) => {
     let { palettes, project_name, id } = project;
-    
-    return <Project palettes={palettes} project_name={project_name} project_id={id} key={'Project'+ index} removeProject={props.removeProject}/>
-  })
-  return (
-    <section className="section__projects--container">
-      {projects}
 
-    </section>
-  );
+    return (
+      <Project
+        palettes={palettes}
+        project_name={project_name}
+        project_id={id}
+        key={'Project' + index}
+        removeProject={props.removeProject}
+      />
+    );
+  });
+  return <section className='section__projects--container'>{projects}</section>;
 }
 
 export default ProjectsContainer;
